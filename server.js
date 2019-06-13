95fcab256a61dc983c215db877bac21b27526e5e
Matt Scheetz <mfscheetz@gmail.com>
2019-06-12 20:11:38 -0400

    tweaks

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const helmet = require('helmet');
 const binanceSvc = require('./services/binance.js');
 const encryptionSvc = require('./services/encryption.js');
 
+const port = process.env.PORT || 8000;
+
 const whitelistOrigins = [
 'http://localhost:4200',
-'http://produrl.com']
+'http://produrl.com'];
 
 var corsOptions = {
   origin: function(origin, callback) {
@@ -19,12 +21,13 @@ var corsOptions = {
   },
   //credentials: true //'http://localhost:8000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-}
+};
 
-app.use(cors(corsOptions))
-app.use(bodyParser.json())
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 app.use(compression());
 app.use(helmet());
+app.set('view engine', 'ejs');
 
 const asyncMiddleware = fn =>
   (req, res, next) => {
@@ -32,13 +35,19 @@ const asyncMiddleware = fn =>
       .catch(next);
   };
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
 app.get('/api/binance/', asyncMiddleware(async function(req, res, next){
   if(!this.headerCheck(req)) {
     res.status(400).json('Invalid request');
   } else {
   	res.status(200).json({'about': 'Collection of binance api calls'});
   }
-}))
+}));
 
 app.get('/api/binance/info', asyncMiddleware(async function(req, res, next){
   if(!this.headerCheck(req)) {
@@ -46,7 +55,7 @@ app.get('/api/binance/info', asyncMiddleware(async function(req, res, next){
   } else {
 	 res.status(200).json(await binanceSvc.getExchangeInfo());
   }
-}))
+}));
 
 app.get('/api/binance/symbols', asyncMiddleware(async function(req, res, next){
   if(!this.headerCheck(req)) {
@@ -55,7 +64,7 @@ app.get('/api/binance/symbols', asyncMiddleware(async function(req, res, next){
   	const data = await binanceSvc.getExchangeInfo();
   	res.status(200).json(data.symbols);
   }
-}))
+}));
 
 app.get('/api/binance/tickers', asyncMiddleware(async function(req, res, next){
   if(!this.headerCheck(req)) {
@@ -63,7 +72,7 @@ app.get('/api/binance/tickers', asyncMiddleware(async function(req, res, next){
   } else {
 	 res.status(200).json(await binanceSvc.getTickers());
   }
-}))
+}));
 
 app.get('/api/binance/klines/:pair/:interval', asyncMiddleware(async function(req, res, next){
   if(!this.headerCheck(req)) {
@@ -74,7 +83,7 @@ app.get('/api/binance/klines/:pair/:interval', asyncMiddleware(async function(re
 
   	res.status(200).json(await binanceSvc.getKlines(pair, interval));
   }
-}))
+}));
 
 const whitelistUsers = new Map([['volitility-d', 'b59e052f-891d-45be-b316-0c22b561bb11'],['volitility-p', 'e64b33f6-54af-4303-9e6e-cc390d2add10']]);
 
@@ -103,8 +112,8 @@ headerCheck = function(req) {
     }
 
     return valid;
-}
+};
 
-app.listen(8000, () => {
-  console.log('Server started!')
-})
+app.listen(port, () => {
+  console.log('Server started on port: '+ port +'!')
+});
