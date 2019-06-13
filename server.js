5a8e2b64f6409d5aab44adf09a581250ccd772bb
Matt Scheetz <mfscheetz@gmail.com>
2019-06-12 20:35:38 -0400

    added index.html

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const asyncMiddleware = fn =>
       .catch(next);
   };
 
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.render('index');
